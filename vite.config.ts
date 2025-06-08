7a4df4ed8b7cb6435572545798eb2b6183e53f90
Fadli <fadli.samaai@gmail.com>
2025-06-09 00:04:50 +0200

    Updated config file and src in the index.html

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "https://github.com/Fadli-Samaai/Portfolio-Website",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
