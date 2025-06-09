deb4a7b194972ae8243f4c45e6bef91d3ff17bd3
Fadli-Samaai <fadli.samaai@gmail.com>
2025-06-09 13:36:13 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: (process.env.NODE_ENV === 'production' || process.env.NODE_ENV ==='development')
+  base: process.env.NODE_ENV === 'production'
     ? "/Portfolio-Website/"
     : '/',
   plugins: [react(), tailwindcss()],
