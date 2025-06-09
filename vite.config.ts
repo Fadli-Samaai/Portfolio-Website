15459b52257027c342a825f4ee1ebea99e8958c8
Fadli <fadli.samaai@gmail.com>
2025-06-09 02:36:36 +0200

    added / back

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from "path"
 // https://vite.dev/config/
 export default defineConfig({
   base: process.env.NODE_ENV === 'production'
-    ? "/Portfolio-Website"
+    ? "/Portfolio-Website/"
     : '/',
   plugins: [react(), tailwindcss()],
   resolve: {
