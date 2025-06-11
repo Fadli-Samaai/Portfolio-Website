def7d0e2b789f7465577a1ea9376341efcce8f07
Fadli <fadli.samaai@gmail.com>
2025-06-11 02:06:16 +0200

    chore- reduced footer hight

--- src/components/FooterSection.tsx
+++ src/components/FooterSection.tsx
@@ -1,8 +1,8 @@
 export const Footer = () =>{
     return (
-<footer className="py-12 px-4 bg-card relative border-t border-border mt-12 pt-8 flex flex-col justify-center items-center gap-4">
+<footer className="py-6 px-4 bg-card relative border-t border-border mt-12 pt-8 flex flex-col justify-center items-center gap-4">
     <p className="text-sm text-muted-foreground">
-        &copy; {new Date().getFullYear()} FadliSamaai.co. All rights reserved.
+        &copy; {new Date().getFullYear()} Fadli Samaai
     </p>
 </footer>
     );
